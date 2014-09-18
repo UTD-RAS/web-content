ed0444d653f74edd695bf42ba6b129692a2d0bde
Michael Aldridge <aldridge.mac@gmail.com>
2014-09-17 20:34:51 -0500

    add makefile and conf file

---
+++ pelicanconf.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'RAS WebMaster'
+SITENAME = u'RAS'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'US/Central'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
